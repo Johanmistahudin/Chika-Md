2b9730b82909ce1b297c48d2b08cd2b3b75b910a
Johanmistahudin <116072035+Johanmistahudin@users.noreply.github.com>
2022-10-18 16:39:32 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 /*
-› Create By @RIY
-› Base Ori @DikaArdnt
+› Create By @johan
+› Base Ori @bot johan
 › Kalau mau tambahin fitur di index.js
 › Edit text menu di file language › help.js
 */
@@ -22,14 +22,14 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6285643291922','6285643291922','6285643291922'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6283862384505'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6283862384505' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Johan' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'bot johan' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'johan' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
